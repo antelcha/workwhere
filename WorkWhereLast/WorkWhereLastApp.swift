9c2e44f864b6a092650e002adbc377a2bbab0dea
mustafath <mustafadalkesen11@gmail.com>
2024-03-19 20:12:45 +0300

    Initial Commit

---
+++ WorkWhereLast/WorkWhereLastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkWhereLastApp.swift
+//  WorkWhereLast
+//
+//  Created by Mustafa Girgin on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkWhereLastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
